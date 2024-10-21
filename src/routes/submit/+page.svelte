af51eb503f91c7026eff608f776c02eb0ef99fbd
Shiven-2007 <shivenag@gmail.com>
2024-10-22 03:53:02 +0530

    file uploading and fixed getting user data

--- src/routes/submit/+page.svelte
+++ src/routes/submit/+page.svelte
@@ -1,241 +1,247 @@
 <script lang="ts">
-    import { PUBLIC_URL } from '$env/static/public';
-    import { browser } from '$app/environment';
-    import type { Tables } from '$lib/types/supabase'
-    import { majors, batches } from '$lib/data/onboardingData';
-    import { slide } from 'svelte/transition';
-	import { goto } from '$app/navigation';
-	import {onMount} from "svelte";
+  import { PUBLIC_URL } from "$env/static/public";
+  import { browser } from "$app/environment";
+  import type { Tables } from "$lib/types/supabase";
+  import { majors, batches } from "$lib/data/onboardingData";
+  import { slide } from "svelte/transition";
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
 
-    export let data;
-    $: ({ supabase, user } = data)
+  export let data;
+  $: ({ supabase, user } = data);
 
-    interface IUserData {
-        user: Tables<'user'>,
-        submission: Tables<'submission'>[]
+  interface IUserData {
+    user: Tables<"user">;
+    submission: Tables<"submission">[];
+  }
+
+  let userData: IUserData | null = null;
+  // user inputs;
+  let fileText: string;
+  let inputRollNumber: string = "";
+  let inputName: string = "";
+  let inputMajor: string = "";
+  let inputBatch: number = 0;
+  let validRollNumber: boolean;
+  let validMajor: boolean;
+  let validBatch: boolean;
+  let submitted: boolean = false;
+
+  let fileContent = "";
+  let fileInput: HTMLInputElement;
+
+  function handleFileInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const files = input.files;
+
+  }
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+  };
+
+  const getUserData = async () => {
+    if (!user) return;
+
+    const response = await fetch(`/api/user?userId=${user.id}`, {
+      method: "GET",
+    });
+
+    const json = await response.json();
+    if (response.status === 200) {
+      userData = json;
+      inputBatch = userData?.user.batch || 0;
+      inputMajor = userData?.user.major || "";
+      inputRollNumber = userData?.user.roll_number || "";
+
+      if (userData?.submission) {
+        submitted = true;
+      }
+    } else {
+      userData = null;
     }
+  };
 
-    let userData: IUserData | null = null;
-    // user inputs;
-	let fileText: string;
-    let inputRollNumber: string = '';
-    let inputName: string = '';
-    let inputMajor: string = '';
-    let inputBatch: number = 0;
-    let validRollNumber: boolean;
-    let validMajor: boolean;
-    let validBatch: boolean;
-	let submitted:boolean = false;
+  $: if (browser && user) {
+    user, getUserData();
+  }
 
-	function handleFileInput(event: Event) {
-        const input = event.target as HTMLInputElement;
-        const files = input.files;
-        
-        if (files && files.length > 0) {
-            const file = files[0];
+  const postFormData = async () => {
+    if (!user) return;
 
-            if (file.name.endsWith(".py")) {
-                const reader = new FileReader();
+    validRollNumber = inputRollNumber !== "";
+    validMajor = inputMajor !== "";
+    validBatch = inputBatch !== 0;
 
-                reader.onload = function() {
-                    fileText = reader.result?.toString() || "";
-                };
+    if (!validRollNumber || !validMajor || !validBatch) return;
 
-                reader.readAsText(file);
-            } else {
-                console.log("Not a Python file");
-            }
-        }
-    }
+	if (fileInput.files && fileInput.files.length > 0) {
+        const file = fileInput.files[0];
+        const { data, error } = await supabase.storage
+            .from("test") // Replace with your bucket name
+            .upload(`uploads/${user?.user_metadata.email.toString().split('@')[0]}/${file.name}`, file);
 
-
-    const handleSignOut = async () => {
-        await supabase.auth.signOut();
-    }
-
-    const getUserData = async () => {
-        if (!user) return;
-
-        const response = await fetch(`/api/user?userId=${user.id}`, {
-            method: 'GET',
-
-        })
-        const json = await response.json();
-
-        if (response.status === 200) {
-            userData = json;
-			if (userData?.submission){
-				submitted=true;
-			}
+        if (error) {
+            console.error("Error uploading file:", error);
+            return; // Exit if there's an error
         } else {
-            userData = null;
-            
+            console.log("File uploaded successfully:", data);
+            fileContent = data.path; // Store the file path for submission
         }
     }
 
-    $: if (browser && user) {
-        user, getUserData();
+    const insertUserData = {
+      id: user.id,
+      name: user.user_metadata.name,
+      email: user.email,
+      roll_number: inputRollNumber,
+      major: inputMajor,
+      batch: inputBatch,
+      submission: fileContent,
+      submission_time: new Date().toISOString(),
+    };
+
+    const response = await fetch("/api/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(insertUserData),
+    });
+    const json = await response.json();
+
+    if (response.status === 201) {
+      submitted = true;
     }
+  };
 
-    const postFormData = async () => {
-        if (!user) return;
-
-        validRollNumber = inputRollNumber !== '';
-        validMajor = inputMajor !== '';
-        validBatch = inputBatch !== 0
-
-        if (!validRollNumber || !validMajor || !validBatch) return;
-
-        const insertUserData = {
-            id: user.id,
-            name: user.user_metadata.name,
-            email: user.email,
-            roll_number: inputRollNumber,
-            major: inputMajor,
-            batch: inputBatch,
-			algorithmFile: fileText,
-			submission: fileContent,
-			submission_time: new Date().toISOString()
-        }
-
-        const response = await fetch('/api/submitalgo', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(insertUserData),
-        })
-        const json = await response.json();
-
-        if (response.status === 201) {
-            submitted=true;
-            
-        }
+  onMount(() => {
+    if (!user) {
+      goto("/profile");
     }
+    console.log(user);
+  });
 
-	onMount(() => {
-		if (!user) {
-			goto('/profile');
-		}
-		console.log(user);
-	});
+  
 
-	let fileContent = '';
-	let fileInput;
-
-	async function handleFileUpload(event: any) {
+  async function handleFileUpload(event: any) {
 		const file = event.target.files[0];
 		if (file) {
-			const text = await file.text();
-			fileContent = text;
-			console.log(fileContent);
+			console.log(file);
 		}
 	}
-
-
 </script>
-
 
 <!-- Form UI -->
 <div class="h-full w-full grid place-items-center">
-	<div
-		class={`absolute top-0 left-0 h-screen w-screen bg-neutral-900/60 grid place-items-center z-50`}
-	>
-		<div
-			transition:slide
-			class={`jetbrains-regular bg-neutral-950 flex flex-col px-6 py-6 rounded-xl`}
-		>
-			<div class={`flex flex-col`}>
-				<p class={`text-2xl md:text-5xl`}>@ping --submission</p>
-				<p class={`text-neutral-500 mt-1 md:text-md text-sm`}>Ready to submit?</p>
-			</div>
+  <div
+    class={`absolute top-0 left-0 h-screen w-screen bg-neutral-900/60 grid place-items-center z-50`}
+  >
+    <div
+      transition:slide
+      class={`jetbrains-regular bg-neutral-950 flex flex-col px-6 py-6 rounded-xl`}
+    >
+      <div class={`flex flex-col`}>
+        <p class={`text-2xl md:text-5xl`}>@ping --submission</p>
+        <p class={`text-neutral-500 mt-1 md:text-md text-sm`}>
+          Ready to submit?
+        </p>
+      </div>
 
-			<div class={`flex flex-col gap-4 text-lg mt-4 md:mx-0 mx-2`}>
-                <div class={`flex flex-row items-center gap-2`}>
-					<p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>
-						--name
-						
-					</p>
-					<input
-						value={user?.user_metadata.name}
-						class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm disabled:opacity-50`}
-						disabled
-					/>
-				</div>
-				<div class={`flex flex-row items-center gap-2`}>
-					<p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>
-						--roll-number
-						{#if validRollNumber === false}
-							<span class={`text-red-600`}>*</span>
-						{/if}
-					</p>
-					<input
-						bind:value={inputRollNumber}
-						class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm disabled:opacity-50`}
-						disabled
-					/>
-				</div>
+      <div class={`flex flex-col gap-4 text-lg mt-4 md:mx-0 mx-2`}>
+        <div class={`flex flex-row items-center gap-2`}>
+          <p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>--name</p>
+          <input
+            value={user?.user_metadata.name}
+            class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm disabled:opacity-50`}
+            disabled
+          />
+        </div>
+        <div class={`flex flex-row items-center gap-2`}>
+          <p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>--Net-ID</p>
+          <input
+            value={user?.user_metadata.email}
+            class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm disabled:opacity-50`}
+            disabled
+          />
+        </div>
 
-				<div class={`flex flex-row items-center gap-2`}>
-					<p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>
-						--major
-						{#if validMajor === false}
-							<span class={`text-red-600`}>*</span>
-						{/if}
-					</p>
-					<select
-						bind:value={inputMajor}
-						class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm disabled:opacity-50`}
-						disabled
-					>
-						{#each majors as major (major.key)}
-							<option value={major.name}>{major.name}</option>
-						{/each}
-					</select>
-				</div>
+        <div class={`flex flex-row items-center gap-2`}>
+          <p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>
+            --roll-number
+            {#if validRollNumber === false}
+              <span class={`text-red-600`}>*</span>
+            {/if}
+          </p>
+          <input
+            bind:value={inputRollNumber}
+            class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm disabled:opacity-50`}
+            disabled
+          />
+        </div>
 
-				<div class={`flex flex-row items-center gap-2`}>
-					<p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>
-						--batch
-						{#if validBatch === false}
-							<span class={`text-red-600`}>*</span>
-						{/if}
-					</p>
-					<select
-						bind:value={inputBatch}
-						class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm disabled:opacity-50`}
-						disabled
-					>
-						{#each batches as batch (batch.key)}
-							<option value={batch.key}>{batch.name}</option>
-						{/each}
-					</select>
-				</div>
-                <div class={`flex flex-row items-center gap-2`}>
-					<p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>
-						--algorithm
-					</p>
-					<input type="file" accept=".py" on:change={handleFileInput} />
-					<input type="file" id="algorithm" name="algorithm" accept=".py" class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm`}
-						   on:change={handleFileUpload}
-						   bind:this={fileInput}
-					/>
-				</div>
-			</div>
+        <div class={`flex flex-row items-center gap-2`}>
+          <p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>
+            --major
+            {#if validMajor === false}
+              <span class={`text-red-600`}>*</span>
+            {/if}
+          </p>
+          <select
+            bind:value={inputMajor}
+            class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm disabled:opacity-50`}
+            disabled
+          >
+            {#each majors as major (major.key)}
+              <option value={major.name}>{major.name}</option>
+            {/each}
+          </select>
+        </div>
 
-			<div class={`grid place-items-center mt-5`}>
-				<button
-					on:click={postFormData}
-					class={`w-fit bg-gradient-to-r from-ping-pink to-ping-orange-primary py-[1px] px-[1px] rounded-2xl`}
-				>
-					<p
-						class={`ubuntu-bol/d jetbrains-regular text-xl bg-neutral-950 hover:bg-neutral-900 active:bg-neutral-900/95 px-3 py-2 rounded-2xl`}
-						
-					>
-						Submit
-					</p>
-				</button>
-			</div>
-		</div>
-	</div>
+        <div class={`flex flex-row items-center gap-2`}>
+          <p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>
+            --batch
+            {#if validBatch === false}
+              <span class={`text-red-600`}>*</span>
+            {/if}
+          </p>
+          <select
+            bind:value={inputBatch}
+            class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm disabled:opacity-50`}
+            disabled
+          >
+            {#each batches as batch (batch.key)}
+              <option value={batch.key}>{batch.name}</option>
+            {/each}
+          </select>
+        </div>
+        <div class={`flex flex-row items-center gap-2`}>
+          <p class={`md:w-[165ps] w-[130px] md:text-md text-sm`}>--algorithm</p>
+
+          <input
+            type="file"
+            id="algorithm"
+            name="algorithm"
+            accept=".py"
+            class={`md:w-[300px] w-[150px] bg-neutral-900 rounded-lg px-2 py-2 text-sm`}
+            on:change={handleFileUpload}
+            bind:this={fileInput}
+          />
+        </div>
+      </div>
+
+      <div class={`grid place-items-center mt-5`}>
+        <button
+          on:click={postFormData}
+          class={`w-fit bg-gradient-to-r from-ping-pink to-ping-orange-primary py-[1px] px-[1px] rounded-2xl`}
+        >
+          <p
+            class={`ubuntu-bol/d jetbrains-regular text-xl bg-neutral-950 hover:bg-neutral-900 active:bg-neutral-900/95 px-3 py-2 rounded-2xl`}
+          >
+            Submit
+          </p>
+        </button>
+      </div>
+    </div>
+  </div>
 </div>
