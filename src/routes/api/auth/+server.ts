9a1727c8ba198d9930f06307b8280f2767aee49d
Lalit M <lalitm1004@gmail.com>
2024-10-23 20:04:37 +0530

    ugh

--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -3,6 +3,7 @@ import { redirect } from "@sveltejs/kit";
 export const GET = async ({ url, locals: { supabase }}) => {
     const code = url.searchParams.get('code');
     if (code) {
+        console.log(code)
         await supabase.auth.exchangeCodeForSession(code);
     }
     redirect(303, '/')
